17c3978274babfa102fe9721ef79f3eb35a274c3
filip7557 <filip.curic@student.ferit.hr>
2024-01-12 03:54:11 +0100

    Add mock data for companies

---
+++ app/src/main/java/hr/ferit/filipcuric/cleantrack/mock/CompaniesMock.kt
@@ -0,0 +1,24 @@
+package hr.ferit.filipcuric.cleantrack.mock
+
+import hr.ferit.filipcuric.cleantrack.model.Company
+
+fun getCompanies(): List<Company> = listOf(
+    Company(
+        id = "asd123",
+        name = "Cleaning Company 1",
+        position = "Manager",
+        imageUrl = "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/cleaning-service-company-logo-design-template-db40091848b1283994ec71ae58270d9f_screen.jpg?ts=1669998818",
+    ),
+    Company(
+        id = "yxc456",
+        name = "Cleaning Company 2",
+        position = "Worker",
+        imageUrl = "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/cleaning-service-company-logo-design-template-db40091848b1283994ec71ae58270d9f_screen.jpg?ts=1669998818",
+    ),
+    Company(
+        id = "qwe678",
+        name = "Cleaning Company 3",
+        position = "Worker",
+        imageUrl = "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/cleaning-service-company-logo-design-template-db40091848b1283994ec71ae58270d9f_screen.jpg?ts=1669998818",
+    )
+)
