c70aace459c65cc72e96afa4337ad792e7413421
filip7557 <filip.curic@student.ferit.hr>
2024-01-17 21:09:35 +0100

    Add register viewmodel

---
+++ app/src/main/java/hr/ferit/filipcuric/cleantrack/ui/register/RegisterViewModel.kt
@@ -0,0 +1,10 @@
+package hr.ferit.filipcuric.cleantrack.ui.register
+
+import androidx.lifecycle.ViewModel
+import hr.ferit.filipcuric.cleantrack.data.UserRepository
+
+class RegisterViewModel(
+    private val userRepository: UserRepository,
+) : ViewModel() {
+
+}
