244ceeeeae367ad165fab5c9f298a0e325c54db7
filip7557 <filip.curic@student.ferit.hr>
2024-01-17 01:48:11 +0100

    Add edit company screen

---
+++ app/src/main/java/hr/ferit/filipcuric/cleantrack/ui/editcompany/EditCompanyScreen.kt
@@ -0,0 +1,113 @@
+package hr.ferit.filipcuric.cleantrack.ui.editcompany
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import hr.ferit.filipcuric.cleantrack.mock.getCompanies
+import hr.ferit.filipcuric.cleantrack.model.Company
+import hr.ferit.filipcuric.cleantrack.ui.component.EditLogoCard
+import hr.ferit.filipcuric.cleantrack.ui.component.EditLogoCardViewState
+import hr.ferit.filipcuric.cleantrack.ui.theme.Green
+
+data class EditCompanyScreenViewState(
+    val company: Company,
+)
+
+@Composable
+fun EditCompanyScreen(
+    editCompanyScreenViewState: EditCompanyScreenViewState,
+    onCreateClick: () -> Unit,
+    onUploadClick: () -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 20.dp)
+            .fillMaxSize()
+    ) {
+        Text(
+            text = "Edit company",
+            fontSize = 18.sp,
+            modifier = Modifier
+                .padding(vertical=12.dp)
+        )
+        TextField(
+            value = "",
+            label = {
+                Text(text = "Name")
+            },
+            singleLine = true,
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Green,
+                focusedLabelColor = Green,
+            ),
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier
+                .padding(bottom = 10.dp)
+                .fillMaxWidth(),
+            onValueChange = { /*TODO*/ }
+        )
+        TextField(
+            value = "",
+            label = {
+                Text(text = "Address")
+            },
+            singleLine = true,
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Green,
+                focusedLabelColor = Green,
+            ),
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier
+                .padding(bottom = 10.dp)
+                .fillMaxWidth(),
+            onValueChange = { /*TODO*/ }
+        )
+        EditLogoCard(
+            editLogoCardViewState = EditLogoCardViewState(
+                imageUrl = editCompanyScreenViewState.company.imageUrl
+            ),
+            onClick = onUploadClick,
+            modifier = Modifier
+                .height(300.dp)
+                .padding(bottom = 10.dp)
+        )
+        Button(
+            onClick = onCreateClick,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0, 90, 4),
+                contentColor = Color.White
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text("Save changes")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun EditCompanyScreenPreview() {
+    EditCompanyScreen(
+        editCompanyScreenViewState = EditCompanyScreenViewState(
+            company = getCompanies().first()
+        ),
+        onCreateClick = { /*TODO*/ }
+    ) {
+
+    }
+}
