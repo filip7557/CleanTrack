bba91bcfff286668b3e2958c32b22f20c51c6438
filip7557 <filip.curic@student.ferit.hr>
2024-01-25 12:43:38 +0100

    Adjust colors

--- app/src/main/java/hr/ferit/filipcuric/cleantrack/ui/login/LoginScreen.kt
+++ app/src/main/java/hr/ferit/filipcuric/cleantrack/ui/login/LoginScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import hr.ferit.filipcuric.cleantrack.ui.theme.Green
