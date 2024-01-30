51421df573594ac911c8acd8730c6ce874a6727e
filip7557 <filip.curic@student.ferit.hr>
2024-01-30 17:35:42 +0100

    Remove unused imports

--- app/src/main/java/hr/ferit/filipcuric/cleantrack/ui/createcompany/CreateCompanyScreen.kt
+++ app/src/main/java/hr/ferit/filipcuric/cleantrack/ui/createcompany/CreateCompanyScreen.kt
@@ -1,6 +1,5 @@
 package hr.ferit.filipcuric.cleantrack.ui.createcompany
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,7 +12,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
