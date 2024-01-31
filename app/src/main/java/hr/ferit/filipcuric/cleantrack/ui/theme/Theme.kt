8acf37d7dadb50d3b3683cff612ec7a6e684078c
filip7557 <filip.curic@student.ferit.hr>
2024-01-31 15:56:20 +0100

    Change status bar color

--- app/src/main/java/hr/ferit/filipcuric/cleantrack/ui/theme/Theme.kt
+++ app/src/main/java/hr/ferit/filipcuric/cleantrack/ui/theme/Theme.kt
@@ -60,7 +60,7 @@ fun CleanTrackTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = Color(1, 81, 5, 0xFF).toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
