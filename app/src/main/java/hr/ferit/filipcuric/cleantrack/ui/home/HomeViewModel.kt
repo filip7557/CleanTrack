1705ba015adfda0fc2122a001f1cd9798b373a9b
filip7557 <filip.curic@student.ferit.hr>
2024-01-31 15:57:23 +0100

    Remove unsused imports

--- app/src/main/java/hr/ferit/filipcuric/cleantrack/ui/home/HomeViewModel.kt
+++ app/src/main/java/hr/ferit/filipcuric/cleantrack/ui/home/HomeViewModel.kt
@@ -1,19 +1,15 @@
 package hr.ferit.filipcuric.cleantrack.ui.home
 
 import android.util.Log
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import hr.ferit.filipcuric.cleantrack.data.repository.CompanyRepository;
-import hr.ferit.filipcuric.cleantrack.data.repository.UserRepository;
+import hr.ferit.filipcuric.cleantrack.data.repository.CompanyRepository
+import hr.ferit.filipcuric.cleantrack.data.repository.UserRepository
 import hr.ferit.filipcuric.cleantrack.model.Company
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class HomeViewModel(
