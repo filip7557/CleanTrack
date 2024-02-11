59925da45e6f58709d521e84e20ab5b7c041e66f
filip7557 <filip.curic@student.ferit.hr>
2024-02-11 23:13:48 +0100

    Change homescreen layout

--- app/src/main/java/hr/ferit/filipcuric/cleantrack/ui/home/HomeScreen.kt
+++ app/src/main/java/hr/ferit/filipcuric/cleantrack/ui/home/HomeScreen.kt
@@ -1,30 +1,27 @@
 package hr.ferit.filipcuric.cleantrack.ui.home
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import hr.ferit.filipcuric.cleantrack.R
 import hr.ferit.filipcuric.cleantrack.navigation.CompanyInfoDestination
 import hr.ferit.filipcuric.cleantrack.ui.component.CompanyCard
 import hr.ferit.filipcuric.cleantrack.ui.component.CompanyCardViewState
-import hr.ferit.filipcuric.cleantrack.ui.component.CreateNewCompanyCard
 import hr.ferit.filipcuric.cleantrack.ui.component.LoadingAnimation
-
-private const val NUMBER_OF_COLUMNS = 2
 
 @Composable
 fun HomeScreen(
@@ -39,48 +36,48 @@ fun HomeScreen(
             viewModel.getCompanies()
             LoadingAnimation()
         } else {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-            ) {
-                Text(
-                    text = stringResource(id = R.string.my_companies),
-                    fontSize = 18.sp,
-                    color = MaterialTheme.colorScheme.tertiary,
-                    modifier = Modifier
-                        .padding(start = 12.dp, top = 12.dp, bottom = 12.dp)
-                )
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(NUMBER_OF_COLUMNS),
-                    contentPadding = PaddingValues(horizontal = 8.dp)
-                ) {
-                    item {
-                        CreateNewCompanyCard(
-                            modifier = Modifier
-                                .height(280.dp)
-                                .width(160.dp)
-                                .padding(vertical = 8.dp, horizontal = 8.dp),
-                            onClick = onCreateClick,
-                        )
-                    }
-
-                    items(
-                        viewModel.companies,
-                        key = { company -> company.id!! }
+            LazyColumn {
+                item {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier
+                            .fillMaxWidth()
                     ) {
-                        CompanyCard(
-                            companyCardViewState = CompanyCardViewState(
-                                name = it.name,
-                                position = if (it.managerId == null || it.managerId != viewModel.userId) "Worker" else "Manager",
-                                imageUrl = it.imageUrl,
-                            ),
+                        Text(
+                            text = stringResource(id = R.string.my_companies),
+                            fontSize = 18.sp,
+                            color = MaterialTheme.colorScheme.tertiary,
                             modifier = Modifier
-                                .height(280.dp)
-                                .width(160.dp)
-                                .padding(vertical = 8.dp, horizontal = 8.dp),
-                            onClick = { onCompanyClick(CompanyInfoDestination.createNavigation(it.id!!)) },
+                                .padding(start = 12.dp, top = 12.dp, bottom = 12.dp)
+                        )
+                        Text(
+                            text = "Create a new company",
+                            fontSize = 18.sp,
+                            color = MaterialTheme.colorScheme.tertiary,
+                            modifier = Modifier
+                                .padding(top = 12.dp, end = 12.dp)
+                                .clickable {
+                                    onCreateClick()
+                                },
+                            textDecoration = TextDecoration.Underline
                         )
                     }
+                }
+                items(
+                    items = viewModel.companies,
+                    key = { company -> company.id!! }
+                ) {
+                    CompanyCard(
+                        companyCardViewState = CompanyCardViewState(
+                            name = it.name,
+                            position = if (it.managerId == null || it.managerId != viewModel.userId) "Worker" else "Manager",
+                            imageUrl = it.imageUrl,
+                        ),
+                        modifier = Modifier
+                            .height(280.dp)
+                            .padding(vertical = 8.dp, horizontal = 8.dp),
+                        onClick = { onCompanyClick(CompanyInfoDestination.createNavigation(it.id!!)) },
+                    )
                 }
             }
         }
